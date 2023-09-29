e1dd20387657a6d795d9ead1e90328a5130bb32d
ead <ead@academico.local>
2023-09-29 11:50:43 -0300

    Inserindo tabela historiaJogo

--- planetAlien.sql
+++ planetAlien.sql
@@ -50,3 +50,26 @@ insert into bloom(nome) values("Criar");
 
 Select * from bloom;
 
+CREATE TABLE historiaJogo(
+id int primary key auto_increment,
+titulo varchar(80),
+descricao longtext,
+final varchar (80));
+
+insert into historiaJogo(descricao) values (
+"No ano de 3250 a terra foi destruída por alienígenas do Planeta Alien. Esses extraterrestes
+eram desenvolvedores de tecnologia avançada em seu planeta, mas aprenderam com os
+humanos a linguagem de marcação HTML, a linguagem de estilização CSS e a linguagem de
+programação Java Script.
+Antes de destruir a terra por inteiro, escolheu um casal de cada Mamífero e fez uma mutação
+genética com a tecnologia do planeta Alien e as tecnologias aprendidas no planeta Terra:
+HTML, CSS e Java Script.
+Todos os Mamíferos mutantes, aceitaram tranquilamente a mutação genética, mas um e
+apenas um, chamado de Furão Mutante Guerreiro, não aceitou. Ele foi levado para o Planeta
+Alien, mas a sua memória ainda continuou na terra. Um dia essas memórias se misturaram
+com sua mutação genética e ele se revoltou contra os aliens.
+A revolta fez nascer missões. E cada missão ganha um nível de força que o torna mais poderoso
+aumentando a possibilidade de destruir mais aliens e assim salvar também os seus amigos
+terrestres.");
+
+select * from historiaJogo;
